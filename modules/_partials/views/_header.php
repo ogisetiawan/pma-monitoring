d2c39f7d882e3ef529392d164a9a5df6e6c8c67c
ogisetiawan <ogisetiawan21@gmail.com>
2019-06-28 11:00:04 +0700

    add :: UI login modals MDB

--- modules/_partials/views/_header.php
+++ modules/_partials/views/_header.php
@@ -2,8 +2,8 @@
   <nav class="navbar navbar-dark red-pma navbar-expand-md sticky-top scrolling-navbar">
       <div class="container-fluid">
           <!-- Brand -->
-          <a class="navbar-brand waves-effect" href="<?= site_url('portal') ?>" target="_blank" style="font-weight:600">
-              <img src="http://192.168.35.160/portal/monitoring/img/blowing.gif" height="30" alt="mdb logo">
+          <a class="navbar-brand waves-effect" href="<?= site_url('') ?>" style="font-weight:600">
+              <img src="http://192.168.35.160/portal/monitoring_old/img/blowing.gif" height="30" alt="Pinus Merah Abadi">
           </a>
 
           <!-- Collapse -->
@@ -12,42 +12,26 @@
           </button>
 
           <!-- Links -->
-          <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent" style="font-size:13px">
+          <div class="collapse navbar-collapse" id="navbarSupportedContent" style="font-size:13px">
               <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active">
-                      <a class="nav-link waves-effect waves-light" href="#">
-                          <i class="fas fa-file-invoice"></i> LBP
-                          <span class="sr-only">(current)</span>
-                      </a>
                   </li>
-                  <li class="nav-item">
-                      <a class="nav-link waves-effect waves-light" href="#">
-                          <i class="fas fa-dollar-sign"></i> KASBANK</a>
-                  </li>
-                  <li class="nav-item">
-                      <a class="nav-link waves-effect waves-light" href="#">
-                          <i class="fas fa-box"></i> INVENTORY</a>
-                  </li>
-                  <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle waves-effect waves-light" id="navbarDropdownMenuLink-4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                            <i class="fas fa-user"></i> USER </a>
-                        <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
-                            <a class="dropdown-item waves-effect waves-light" href="#">My account</a>
-                            <a class="dropdown-item waves-effect waves-light" href="#">Log out</a>
-                        </div>
-                    </li>
+                  <a id="navbar-static-login" class="btn btn-outline-light btn-sm" data-toggle="modal" style="border-radius:16px" data-target="#modalLogin">
+                      Log In
+                      <i class="fas fa-sign-in-alt"></i>
+                  </a>
               </ul>
-          </div> -->
+          </div>
       </div>
   </nav>
   <!--/.Navbar-->
 
+  <!-- Header Title -->
   <div class="d-flex justify-content-between shadow-sm p-2">
       <div>
           <div class="btn-group btn-group-sm" role="group" aria-label="Basic example">
-            <button type="button" onClick="fnExcelReport()" class="btn btn-outline"><i class="fas fa-file-excel pr-2"></i>XLSX</button>
-            <button type="button" class="btn btn-outline"><i class="far fa-file-pdf pr-2"></i>PDF</button>
-            <button type="button" class="btn btn-outline"><i class="fas fa-print pr-2"></i>PRINT</button>
+              <button type="button" onClick="fnExcelReport()" class="btn btn-outline"><i class="fas fa-file-excel pr-2"></i>XLSX</button>
+              <button type="button" class="btn btn-outline"><i class="far fa-file-pdf pr-2"></i>PDF</button>
+              <button type="button" class="btn btn-outline"><i class="fas fa-print pr-2"></i>PRINT</button>
           </div>
       </div>
       <div class="">
@@ -62,9 +46,9 @@
               <div class="col-auto col-xs-12">
                   <select id="selected-bulan" class="form-control custom-select-sm"" title=" Bulan">
                       <?php
-                          $tahun = date('Y') + 1;
-                          $month = date('m');
-                      ?>
+                        $tahun = date('Y') + 1;
+                        $month = date('m');
+                        ?>
                       <option <?php if ($month == '01') {
                                     echo "selected ";
                                 } ?>value="01"> January</option>
@@ -113,7 +97,7 @@
                       <?php } ?>
                   </select>
               </div>
-              
+
               <div class="col-auto col-xs-12">
                   <select id="selected-group-region" class="form-control custom-select-sm" title=" Group Region">
                       <option value="" selected>-- SELECT GROUP REGION --</option>
@@ -131,8 +115,63 @@
       </div>
   </div>
   <section class="text-center mt-4">
-    <h4 class="font-medium font-weight-light text-uppercase">
-        <span class="bq-reds pl-1 title">
-        </span>
-    </h4>
-</section>
+      <h4 class="font-medium font-weight-light text-uppercase">
+          <span class="bq-reds pl-1 title">
+          </span>
+      </h4>
+  </section>
+  <!-- Header Title -->
+
+
+  <!-- Modal: modalPoll -->
+  <div class="modal fade right" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="modalLogin" aria-hidden="true" data-backdrop="false">
+      <div class="modal-dialog modal-full-height modal-right modal-notify modal-info" role="document">
+          <div class="modal-content">
+              <!--Header-->
+              <div class="modal-header" style="background: -webkit-linear-gradient(right, #f05454 0%, #cc0000 65%);">
+
+                  <p class="heading lead text-uppercase">dashboard monitoring</p>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true" class="white-text">×</span>
+                  </button>
+              </div>
+              <!--Body-->
+              <div class="modal-body">
+                  <!-- Material form register -->
+                  <form>
+                      <!-- Material outline input with prefix-->
+                      <p class="h4 mb-4"><i class="fas fa-sign-in-alt animated rotateIn mr-2"></i>Login Form</p>
+                      <div class="md-form md-outline">
+                          <i class="fas fa-envelope prefix grey-text"></i>
+                          <input type="text" id="inputIconEx1" class="form-control">
+                          <label for="inputIconEx1" data-error="wrong" data-success="right">E-mail address</label>
+                      </div>
+                      <div class="md-form md-outline">
+                          <i class="fas fa-lock prefix grey-text"></i>
+                          <input type="password" id="inputValidationEx2" class="form-control validate">
+                          <label for="inputValidationEx2" data-error="wrong" data-success="right">Your password</label>
+                      </div>
+                      <div class="d-flex justify-content-around">
+                          <div>
+                              <!-- Remember me -->
+                              <div class="custom-control custom-checkbox">
+                                  <input type="checkbox" class="custom-control-input" id="defaultLoginFormRemember">
+                                  <label class="custom-control-label" for="defaultLoginFormRemember">Remember me</label>
+                              </div>
+                          </div>
+                          <div>
+                              <!-- Forgot password -->
+                              <a href="">Forgot password?</a>
+                          </div>
+                      </div>
+                      <div class="text-center mt-4">
+                          <button class="btn btn-sm btn-danger text-uppercase" type="submit">Login</button>
+                          <button class="btn btn-sm btn-cyan text-uppercase" data-dismiss="modal" type="submit">Cancel</button>
+                      </div>
+                  </form>
+                  <!-- Material form register -->
+              </div>
+          </div>
+      </div>
+  </div>
+  <!-- Modal: modalPoll -->
