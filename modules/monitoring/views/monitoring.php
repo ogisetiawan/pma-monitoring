d3093943d207a38a6d41556adbdc9d8ee669bc5c
ogisetiawan <ogisetiawan21@gmail.com>
2019-07-17 10:22:07 +0700

    update :: curl export xls

--- modules/monitoring/views/monitoring.php
+++ modules/monitoring/views/monitoring.php
@@ -283,39 +283,39 @@ $month = date('m');
     }
     // ! eventReport
     function fnExcelReport() {
-        // thn = $('#selected-tahun').val();
-        // bln = $('#selected-bulan').val();
-        // window.location = "http://192.168.35.160/portal/monitoring_old/export/export_excel_LBP_all.php"+"?thn="+thn+"&bln="+bln;
-        var tab_text = "<table border='2px'><tr bgcolor='#87AFC6'>";
-        var textRange;
-        var j = 0;
-        tab = document.getElementById('table-monitoring'); // id of table
+        thn = $('#selected-tahun').val();
+        bln = $('#selected-bulan').val();
+        window.location = "http://192.168.35.160/portal/monitoring_old/export/export_excel_LBP_all.php"+"?thn="+thn+"&bln="+bln;
+        // var tab_text = "<table border='2px'><tr bgcolor='#87AFC6'>";
+        // var textRange;
+        // var j = 0;
+        // tab = document.getElementById('table-monitoring'); // id of table
 
-        for (j = 0; j < tab.rows.length; j++) {
-            tab_text = tab_text + tab.rows[j].innerHTML + "</tr>";
-            //tab_text=tab_text+"</tr>";
-        }
+        // for (j = 0; j < tab.rows.length; j++) {
+        //     tab_text = tab_text + tab.rows[j].innerHTML + "</tr>";
+        //     //tab_text=tab_text+"</tr>";
+        // }
 
-        tab_text = tab_text + "</table>";
-        tab_text = tab_text.replace(/<A[^>]*>|<\/A>/g, ""); //remove if u want links in your table
-        tab_text = tab_text.replace(/<img[^>]*>/gi, ""); // remove if u want images in your table
-        tab_text = tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
-        tab_text = tab_text.replace(/•/g, "<p style='font-size:15px'>&#149</p>");
-        tab_text = tab_text.replace(/urgent/g, "<p style='color:red'>URGENT</p>");
-        tab_text = tab_text.replace(/complete/g, "<p style='color:red'>COMPLETE</p>");
-        var ua = window.navigator.userAgent;
-        var msie = ua.indexOf("MSIE ");
+        // tab_text = tab_text + "</table>";
+        // tab_text = tab_text.replace(/<A[^>]*>|<\/A>/g, ""); //remove if u want links in your table
+        // tab_text = tab_text.replace(/<img[^>]*>/gi, ""); // remove if u want images in your table
+        // tab_text = tab_text.replace(/<input[^>]*>|<\/input>/gi, ""); // reomves input params
+        // tab_text = tab_text.replace(/•/g, "<p style='font-size:15px'>&#149</p>");
+        // tab_text = tab_text.replace(/urgent/g, "<p style='color:red'>URGENT</p>");
+        // tab_text = tab_text.replace(/complete/g, "<p style='color:red'>COMPLETE</p>");
+        // var ua = window.navigator.userAgent;
+        // var msie = ua.indexOf("MSIE ");
 
-        if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer
-        {
-            txtArea1.document.open("txt/html", "replace");
-            txtArea1.document.write(tab_text);
-            txtArea1.document.close();
-            txtArea1.focus();
-            sa = txtArea1.document.execCommand("SaveAs", true, "Say Thanks to Sumit.xls");
-        } else //other browser not tested on IE 11
-            sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
-        return (sa);
+        // if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) // If Internet Explorer
+        // {
+        //     txtArea1.document.open("txt/html", "replace");
+        //     txtArea1.document.write(tab_text);
+        //     txtArea1.document.close();
+        //     txtArea1.focus();
+        //     sa = txtArea1.document.execCommand("SaveAs", true, "Say Thanks to Sumit.xls");
+        // } else //other browser not tested on IE 11
+        //     sa = window.open('data:application/vnd.ms-excel,' + encodeURIComponent(tab_text));
+        // return (sa);
     }
 
     // ! onEventDocument
