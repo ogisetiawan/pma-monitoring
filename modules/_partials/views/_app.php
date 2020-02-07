fa9681763e8047741a7dc3c61be4f8c9306d8c5f
Ogi Setiawan <ogisetaiwan21@gmail.com>
2020-02-07 13:48:09 +0700

    style mininfy

--- modules/_partials/views/_app.php
+++ modules/_partials/views/_app.php
@@ -22,7 +22,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/amaranjs@0.5.5/dist/css/amaran.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/amaranjs@0.5.5/dist/css/animate.min.css" rel="stylesheet">
-    <link href="<?= base_url('assets/css/style.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/style.min.css') ?>" rel="stylesheet">
     <!-- Main JS -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
